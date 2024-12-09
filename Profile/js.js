839408e776f7665105acbf7958226492783c0245
Vansh Saini <124070153+vansh16saini@users.noreply.github.com>
2024-12-09 11:41:57 +0530

    Js update

--- Profile/js.js
+++ Profile/js.js
@@ -58,6 +58,21 @@ function type() {
         setTimeout(type, isDeleting ? deletingSpeed : typingSpeed);
     }
 }
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
+buttons.forEach(button =>{
+    button.addEventListener('click', (e) => {
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
 
 // Start the typing effect
 type();
